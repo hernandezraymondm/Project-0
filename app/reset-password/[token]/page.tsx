00071d32927bdc0aad7b48c8aad8cfb02e15cf64
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-27 15:51:09 +0800

    follow documentation dynamic route best practice

--- app/reset-password/[token]/page.tsx
+++ app/reset-password/[token]/page.tsx
@@ -1,14 +1,15 @@
 import { NewPasswordForm } from "@/components/new-password-form";
 
-export default function ResetPasswordPage({
+export default async function ResetPasswordPage({
   params,
 }: {
   params: { token: string };
 }) {
+  const token = (await params).token;
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-2xl font-bold mb-8">Set New Password</h1>
-      <NewPasswordForm token={params.token} />
+      <NewPasswordForm token={token} />
     </div>
   );
 }
--- app/verify-email/[token]/page.tsx
+++ app/verify-email/[token]/page.tsx
@@ -1,14 +1,15 @@
 import { VerifyEmail } from "@/components/verify-email";
 
-export default function VerifyEmailPage({
+export default async function VerifyEmailPage({
   params,
 }: {
   params: { token: string };
 }) {
+  const token = (await params).token;
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-2xl font-bold mb-8">Verify Email</h1>
-      <VerifyEmail token={params.token} />
+      <VerifyEmail token={token} />
     </div>
   );
 }
